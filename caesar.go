b291b23ba1cda79a73da5a0534ec1c9768a83781
Brian Kelly <brian.kelly@autodesk.com>
2021-01-24 18:55:55 -0800

    add comments

--- caesar.go
+++ caesar.go
@@ -1,7 +1,10 @@
+// Package caesar provides a caesar cipher for encoding & decoding strings
 package caesar
 
 const Shift = 3
 
+// Encodes shifts each alphabetic character in the string 3 places to the right.
+// If the character is not a letter it does nothing
 func Encode(text string) string {
 	letters := []rune(text)
 
@@ -16,6 +19,8 @@ func Encode(text string) string {
 	return string(letters)
 }
 
+// Decode shifts each alphabetic character in the string 3 places to the left.
+// If the character is not a letter it does nothing
 func Decode(text string) string {
 	letters := []rune(text)
 
